245dea0a2c29ce9ecf787ebc60364ef337e04e9e
unknown <2305461452@qq.com>
2019-05-17 15:29:33 +0800

    Initial commit

---
+++ src/h22.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: h22
+ * Author:   hxq
+ * Date:     2019/5/17 15:23
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author hxq
+ * @create 2019/5/17
+ * @since 1.0.0
+ */
+public class h22 {
+
+}
+
